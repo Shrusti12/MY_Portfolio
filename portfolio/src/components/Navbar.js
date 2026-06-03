fbaebfcd658d1ee0968e0a8cdd1b203cb5371976
Shrusti Mathapati <shrustimathapati2003@gmail.com>
2026-06-03 12:08:46 +0530

    Initial portfolio website

---
+++ portfolio/src/components/Navbar.js
@@ -0,0 +1,36 @@
+import React from "react";
+import "../styles/Navbar.css";
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="logo">
+        Shrusti<span>.</span>
+      </div>
+
+      <ul className="nav-links">
+        <li>
+          <a href="#home">Home</a>
+        </li>
+
+        <li>
+          <a href="#about">About</a>
+        </li>
+
+        
+
+        <li>
+          <a href="#projects">Projects</a>
+        </li>
+
+       
+      </ul>
+
+      <button className="hire-btn">
+        Hire Me
+      </button>
+    </nav>
+  );
+}
+
+export default Navbar;
