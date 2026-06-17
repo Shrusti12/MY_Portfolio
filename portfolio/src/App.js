30775941cb1ebcd5d0272ca0194cd508681f93a6
Shrusti Mathapati <shrustimathapati2003@gmail.com>
2026-06-17 21:24:12 +0530

    Updated portfolio home page and animations

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,11 +1,28 @@
+import { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
+
 import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import About from "./components/About";
 
 import "./App.css";
 
 function App() {
+  useEffect(() => {
+    AOS.init({
+      duration: 1000,
+      once: true,
+    });
+  }, []);
+  
   return (
     <>
       <Navbar />
+      <main>
+        <Hero />
+        <About />
+      </main>
     </>
   );
 }
